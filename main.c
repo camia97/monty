c54b5d6accda5086c4595255c1a45c4a38dcc5dc
camia97 <3962@holbertonschool.com>
2022-01-13 08:26:18 -0800

    fix print output

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	op = fopen(argv[1], "r");
 	if (!op)
 	{
-		fprintf(stderr, "Error: Can't open file <argv[1]>");
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&buffer, &size, op) != -1)
